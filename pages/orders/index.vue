ade732cd613503eb5aef090ee50571457e533957
Ricardo <ric.dalvarez@hotmail.com>
2023-10-11 12:53:15 +0200

    add style rendering list

--- pages/orders/index.vue
+++ pages/orders/index.vue
@@ -12,11 +12,65 @@ const { data } = await find<OrderListResponse>('orders', {
   }
 });
 const orders = ref(data);
+console.log({orders})
 </script>
 
 <template>
-  <section class="p-3">
+ <section class="p-3 order-list">
     <h1 class="text-center">Order List</h1>
-    <!-- Write rest of the code here -->
+    <div class="filter">
+
+      <h2>filters</h2> 
+      <select name="cars" id="cars">
+        <option value="all">all</option>
+        <option value="normal">normal</option>
+        <option value="donation">donation</option>
+      </select>
+    </div>
+
+    <div v-if="orders && orders.length > 0">
+      <div v-for="order in orders" :key="order.id" class="order">
+        <h3>{{ order.id }}</h3>
+        <!-- TODO nest skus -->
+        <h3>sku: {{ order.attributes.order_items.data[0].attributes.sku }}</h3>
+        <h3>type: {{ order.attributes.type}}</h3>
+        <h3>quantity: {{ order.attributes.order_items.data[0].attributes.quantity}}</h3>
+        <h3>firstname: {{ order.attributes.order_meta.data.attributes.shipping_firstname }}</h3>
+       
+      </div>
+    </div>
+    <div v-else>
+      <p>No orders found.</p>
+    </div>
   </section>
 </template>
+
+<style scoped>
+
+.filter {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1rem;
+}
+
+.order-list {
+  max-width: 800px;
+  margin: 0 auto;
+
+}
+
+.order {
+  border: 1px solid #ccc;
+  padding: 1rem;
+  margin: 1rem;
+  background-color: #f7f7f7;
+}
+
+.order h1 {
+  font-size: 1.5rem;
+  color: #333;
+}
+
+
+</style>
